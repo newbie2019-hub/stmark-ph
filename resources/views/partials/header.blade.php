fc133eccf32080b4c672db9a6c4c5384b1da75c6
yvansabay <sabayyvan2018@gmail.com>
2020-09-25 15:50:24 +0800

    Added Meta Description

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,12 @@
 <meta charset="utf-8">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="The first Coptic Orthodox Church in the Philippines ministering the Word of God and serving
+congregants worldwide through it’s websites and TV programs." />
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <meta name="google-site-verification" content="bg8CVWdiUaNLqzF1NsZdI14H1-z9HF9ZC9JkDGETZwo" />
+
 <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 <title>St. Mark Coptic Orthodox Church, Philippines</title>
 <script>
